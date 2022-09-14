a144d8b03cec30473feae887ac00acc97fb6348c
VTarunaKumar <vtarunakumarrao>
2022-09-14 18:36:13 +0530

    Doing

--- lib/screens/LoadingScreen.dart
+++ lib/screens/LoadingScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:mausam/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,10 +9,15 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  getLocation() async {
+    Location location = Location();
+    await location.getCurrrentLocation();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
