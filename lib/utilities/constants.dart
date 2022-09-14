5506e11ff930b10bd5fa5d019b101ab6a9560eb9
VTarunaKumar <vtarunakumarrao>
2022-09-14 17:08:39 +0530

    Restart

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+const kMessageTextStyle = TextStyle(
+  fontSize: 60,
+  fontFamily: 'Spartan MB',
+);
+const kButtonTextStyle = TextStyle(
+  fontSize: 30,
+  fontFamily: 'Spartan MB',
+);
+const kConditionTextStyle = TextStyle(
+  fontSize: 100,
+);
